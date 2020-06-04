6cba8ccf0f398260557b570564bb60e89a6c66d4
Taras Bandrivskyy <bandrivtara@gmail.com>
2020-06-04 14:30:49 +0200

    Updates

---
+++ precache-manifest.0a70dfd7c8fb1250954108800149d89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4823d76dacbb6a85b86dffea3a3ad3",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "48d3d718930308c60d22",
+    "url": "/currency-converter-react/static/css/main.761ad1cf.chunk.css"
+  },
+  {
+    "revision": "b27eb492d1ea269d6eac",
+    "url": "/currency-converter-react/static/js/2.affe1f80.chunk.js"
+  },
+  {
+    "revision": "1d38b63b6ff5da75cc37a6221e6de215",
+    "url": "/currency-converter-react/static/js/2.affe1f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d3d718930308c60d22",
+    "url": "/currency-converter-react/static/js/main.9614e0b6.chunk.js"
+  },
+  {
+    "revision": "d068b1d20e25d9580d58",
+    "url": "/currency-converter-react/static/js/runtime-main.d21c921e.js"
+  }
+]);
